ec0b9970bb035d428e9a491b4ff32d78ff50590a
Alexander W Blocker <ablocker@fas.harvard.edu>
2013-01-18 17:55:24 -0500

    Rewrite to use optparse instead of getopt.

--- scripts/mk_wavelet_basis.R
+++ scripts/mk_wavelet_basis.R
@@ -6,52 +6,45 @@ if (!suppressMessages(require(wavethresh, quietly=TRUE))) {
   q(status=1)
 }
 
-if(!suppressMessages(require(getopt, quietly=TRUE))) {
-  cat("Error - getopt library is required.\n", file=stderr())
+if(!suppressMessages(require(optparse, quietly=TRUE))) {
+  cat("Error - optparse library is required.\n", file=stderr())
   q(status=1)
 }
 
 
 # Set constants
 
-kDefaults <- list(filter.family='DaubLeAsymm',
-                  filter.number=4,
-                  bc='symmetric',
-                  B=2048,
-                  sep=' ',
-                  help=FALSE)
+kOptList <- list(
+  make_option(c('--filter.family', '-f'), default='DaubLeAsymm',
+              help=paste('A filter family recognized by wavethresh.',
+                         '\n\t\tDefaults to %default.', sep='')),
+  make_option(c('--filter.number', '-i'), type='integer', default=4,
+              help=paste('Smoothness of wavelet to use for basis.',
+                         '\n\t\tDefaults to %default.', sep='')),
+  make_option(c('--boundary', '-b'), default='symmetric',
+              help=paste('Boundary handling setting.',
+                         '\n\t\tDefaults to %default.', sep='')),
+  make_option(c('--B', '-B'), type='integer', default=2048,
+              help=paste('Size of basis to construct.',
+                         '\n\t\tDefaults to %default.', sep='')),
+  make_option(c('--c', '-c'), type='integer', default=0,
+              help=paste('Number of columns of basis to output (starting from ',
+                         'lowest\n\t\tfrequencies). Defaults to B.', sep='')),
+  make_option(c('--sep', '-s'), default=' ',
+              help=paste('Separator for output.',
+                         '\n\t\tDefaults to %default.', sep=''))
+)
 
-kOptSpec <- matrix(c(
-  'filter.family', 'f', 1, 'character',
-  'filter.number', 'i', 1, 'integer',
-  'bc', 'b', 1, 'character',
-  'B', 'B', 1, 'integer',
-  'n', 'n', 1, 'integer',
-  'sep', 's', 1, 'character',
-  'help', 'h', 0, 'logical'),
-  ncol=5, byrow=TRUE)
-
-kHelp <- '
-Usage: Rscript mk_wavelet_basis.R [options] [> basis.txt]
-  -f|--filter.family    A filter family recognized by wavethresh.
-                        Defaults to DaubLeAsymm.
-  -i|--filter.number    Smoothness of wavelet to use for basis.
-                        Defaults to 4.
-  -b|--bc               Boundary handling.
-                        Defaults to symmetric.
-  -B                    Size of basis to construct. Must be a power of 2.
-                        Defaults to 2048.
-  -n                    Number of columns of basis to output (starting from
-                        lowest frequencies). Defaults to B.
-  -s|--sep              Separator for output. Defaults to " ".
-  -h|--help             Display this help message and exit.
-
+kUsage <- 'Usage: Rscript mk_wavelet_basis.R [options] [> basis.txt]'
+kEpilogue <- '
 Constructs basis matrix corresponding to given wavelet filter, outputting the
 first n columns to stdout as SEP-separated text.
-
-Requires the wavethresh and getopt R packages (available on CRAN).
+Requires the wavethresh and optparse R packages (available on CRAN).
 
 '
+
+kParser <- OptionParser(usage=kUsage, option_list=kOptList,
+                        epilogue=kEpilogue)
 
 
 # Function definitions
@@ -94,35 +87,24 @@ basis.reconstruct <- function(wt) {
 # Script
 
 # Parse options
-opts <- getopt(kOptSpec)
+optargs <- parse_args(kParser, commandArgs(TRUE), positional_arguments=TRUE) 
+opts <- optargs$options
 
-# Set defaults, if needed
-for (oo in names(kDefaults)) {
-  if (!(oo %in% names(opts)))
-    opts[[oo]] <- kDefaults[[oo]]
-}
-
-# Handle n separately
-if (!('n' %in% names(opts)))
-  opts$n <- opts$B
-
-# Display help message and exit, if requested
-if (opts$help) {
-  cat(kHelp)
-  q(status=1)
-}
+# Handle c separately
+if (opts$c < 1)
+  opts$c <- opts$B
 
 
 # Build zero sequence and base wt
 y <- rep(0, opts$B)
 wt <- wd(y, filter.number=opts$filter.number, family=opts$filter.family,
-         bc=opts$bc)
+         bc=opts$boundary)
 
 # Reconstruct wavelet basis
 X <- basis.reconstruct(wt)
 
 
 # Write basis to stdout
-write.table(X[, 1:opts$n], file=stdout(), sep=opts$sep,
+write.table(X[, 1:opts$c], file=stdout(), sep=opts$sep,
             row.names=FALSE, col.names=FALSE)
 
